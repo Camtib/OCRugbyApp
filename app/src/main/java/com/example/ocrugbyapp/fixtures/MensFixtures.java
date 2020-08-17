9c1544b9c532256eafc849eddc044527a7889aea
Nat Lubbock <natlubbock@Nats-MacBook-Air.local>
2020-08-17 15:45:22 +0100

    added code to MensFixtures.java to get fixture information from Firebase Firstore.

--- app/src/main/java/com/example/ocrugbyapp/fixtures/MensFixtures.java
+++ app/src/main/java/com/example/ocrugbyapp/fixtures/MensFixtures.java
@@ -1,5 +1,7 @@
 package com.example.ocrugbyapp.fixtures;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,17 +9,26 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ocrugbyapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.FileAsyncHttpResponseHandler;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
 import jxl.Sheet;
@@ -30,14 +41,205 @@ public class MensFixtures extends Fragment {
     private static final String TAG = "MenFixtures";
 
     private RecyclerView mRecyclerView;
-    AsyncHttpClient client;
-    Workbook workbook;
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
+    MensFixturesListAdapter mensFixturesListAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_mens_fixtures, container, false);
+        final View view = inflater.inflate(R.layout.fragment_mens_fixtures, container, false);
+
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recyclerViewMensFixtures);
+
+        mAuth = FirebaseAuth.getInstance();
+        mStore = FirebaseFirestore.getInstance();
+
+        mStore.collection("ocrfcFixtures").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                if (task.isSuccessful()) {
+                    List<MensFixtureCard> fixture = new ArrayList<>();
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if (document != null) {
+
+                            final String date, firstsOpponent, firstsHA, firstsLC, firstsKO, firstsMeet, firstsAddress, firstsPostcode;
+                            final String secondsOpponent, secondsHA, secondsLC, secondsKO, secondsMeet, secondsAddress, secondsPostcode;
+                            final String bsOpponent, bsHA, bsLC, bsKO, bsMeet, bsAddress, bsPostcode;
+
+                            date = document.get("date").toString();
+
+                            if (!document.get("firstsOpponent").toString().equals("")) {
+                                firstsOpponent = "1st XV vs " + document.get("firstsOpponent").toString();
+                            } else {
+                                firstsOpponent = "No Fixture";
+                            }
+
+                            if (document.get("firstsHA").toString().equals("H")) {
+                                firstsHA = "Home";
+                            } else if (document.get("firstsHA").toString().equals("A")) {
+                                firstsHA = "Away";
+                            } else {
+                                firstsHA = document.get("firstsHA").toString();
+                            }
+
+                            if (document.get("firstsLC").toString().equals("L")) {
+                                firstsLC = "League Match";
+                            } else if (document.get("firstsLC").toString().equals("C")) {
+                                firstsLC = "Cup Match";
+                            } else {
+                                firstsLC = document.get("firstsLC").toString();
+                            }
+
+                            firstsKO = "Kick Off: " + document.get("firstsKO").toString();
+                            firstsMeet = "Meet Time: " + document.get("firstsMeet").toString();
+                            firstsAddress = document.get("firstsAddress").toString();
+                            firstsPostcode = document.get("firstsPostcode").toString();
+
+
+
+                            if (!document.get("secondsOpponent").toString().equals("")) {
+                                secondsOpponent = "2nd XV vs " + document.get("secondsOpponent").toString();
+                            } else {
+                                secondsOpponent = "No Fixture";
+                            }
+
+                            if (document.get("secondsHA").toString().equals("H")) {
+                                secondsHA = "Home";
+                            } else if (document.get("secondsHA").toString().equals("A")) {
+                                secondsHA = "Away";
+                            } else {
+                                secondsHA = document.get("secondsHA").toString();
+                            }
+
+                            if (document.get("secondsLC").toString().equals("L")) {
+                                secondsLC = "League Match";
+                            } else if (document.get("secondsLC").toString().equals("C")) {
+                                secondsLC = "Cup Match";
+                            } else {
+                                secondsLC = document.get("secondsLC").toString();
+                            }
+
+                            secondsKO = "Kick Off: " + document.get("secondsKO").toString();
+                            secondsMeet = "Meet Time: " + document.get("secondsMeet").toString();
+                            secondsAddress = document.get("secondsAddress").toString();
+                            secondsPostcode = document.get("secondsPostcode").toString();
+
+
+
+                            if (!document.get("bsOpponent").toString().equals("")) {
+                                bsOpponent = "B XV vs " + document.get("bsOpponent").toString();
+                            } else {
+                                bsOpponent = "No Fixture";
+                            }
+
+                            if (document.get("bsHA").toString().equals("H")) {
+                                bsHA = "Home";
+                            } else if (document.get("bsHA").toString().equals("A")) {
+                                bsHA = "Away";
+                            } else {
+                                bsHA = document.get("bsHA").toString();
+                            }
+
+                            if (document.get("bsLC").toString().equals("L")) {
+                                bsLC = "League Match";
+                            } else if (document.get("bsLC").toString().equals("C")) {
+                                bsLC = "Cup Match";
+                            } else {
+                                bsLC = document.get("bsLC").toString();
+                            }
+
+                            bsKO = "Kick Off: " + document.get("bsKO").toString();
+                            bsMeet = "Meet Time: " + document.get("bsMeet").toString();
+                            bsAddress = document.get("bsAddress").toString();
+                            bsPostcode = document.get("bsPostcode").toString();
+
+                            fixture.add(new MensFixtureCard(date, firstsOpponent, firstsHA, firstsLC, firstsKO, firstsMeet, firstsAddress, firstsPostcode,
+                                    secondsOpponent, secondsHA, secondsLC, secondsKO, secondsMeet, secondsAddress, secondsPostcode,
+                                    bsOpponent, bsHA, bsLC, bsKO, bsMeet, bsAddress, bsPostcode));
+
+
+
+                            /*
+
+                            class SaveFixture extends AsyncTask<Void, Void, Void> {
+
+                                @Override
+                                protected Void doInBackground(Void... voids) {
+
+                                    MensFixtureCard fixture = new MensFixtureCard(date, firstsOpponent, firstsHA, firstsLC, firstsKO, firstsMeet, firstsAddress, firstsPostcode,
+                                            secondsOpponent, secondsHA, secondsLC, secondsKO, secondsMeet, secondsAddress, secondsPostcode,
+                                            bsOpponent, bsHA, bsLC, bsKO, bsMeet, bsAddress, bsPostcode);
+
+                                    fixture.setExpandable(false);
+
+
+                                    fixture.setDate(date);
+                                    fixture.setFirstsFixture(firstsOpponent);
+                                    fixture.setFirstsLC(firstsLC);
+                                    fixture.setFirstsHA(firstsHA);
+                                    fixture.setFirstsKO(firstsKO);
+                                    fixture.setFirstsMeet(firstsMeet);
+                                    fixture.setFirstsAddress(firstsAddress);
+                                    fixture.setFirstsPostcode(firstsPostcode);
+
+                                    fixture.setSecondsFixture(secondsOpponent);
+                                    fixture.setSecondsLC(secondsLC);
+                                    fixture.setSecondsHA(secondsHA);
+                                    fixture.setSecondsKO(secondsKO);
+                                    fixture.setSecondsMeet(secondsMeet);
+                                    fixture.setSecondsAddress(secondsAddress);
+                                    fixture.setSecondsPostcode(secondsPostcode);
+
+                                    fixture.setBsFixture(bsOpponent);
+                                    fixture.setBsLC(bsLC);
+                                    fixture.setBsHA(bsHA);
+                                    fixture.setBsKO(bsKO);
+                                    fixture.setBsMeet(bsMeet);
+                                    fixture.setBsAddress(bsAddress);
+                                    fixture.setBsPostcode(bsPostcode);
+
+
+                                    fixture.setExpandable(false);
+
+                                    FixtureDatabaseClient.getInstance(getApplicationContext()).getAppDatabase()
+                                            .fixtureDao()
+                                            .insertFixtures(fixture);
+
+
+
+
+                                    return null;
+                                }
+
+                                @Override
+                                protected void onPostExecute(Void aVoid) {
+                                    super.onPostExecute(aVoid);
+                                    finish();
+
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+
+                                }
+                            }
+
+                            SaveFixture sf = new SaveFixture();
+                            sf.execute();
+
+                             */
+
+
+                        }
+                    }
+
+                    mensFixturesListAdapter = new MensFixturesListAdapter(view.getContext(), fixture);
+                    mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+                    mRecyclerView.setAdapter(mensFixturesListAdapter);
+                }
+
+            }
+        });
 
 
         return view;
