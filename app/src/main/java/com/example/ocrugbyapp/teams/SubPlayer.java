b0eb823700dda243da8625f9b20e0922f9b98665
Nat Lubbock <natlubbock@Nats-MacBook-Air.local>
2020-09-03 12:32:52 +0100

    added SubPlayer.java

---
+++ app/src/main/java/com/example/ocrugbyapp/teams/SubPlayer.java
@@ -0,0 +1,18 @@
+package com.example.ocrugbyapp.teams;
+
+public class SubPlayer {
+
+    private String name;
+
+    public SubPlayer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
