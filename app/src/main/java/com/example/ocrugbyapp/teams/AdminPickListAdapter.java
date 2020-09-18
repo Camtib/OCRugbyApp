a479a8234dc786d0a9d9bdaae2eb5725aa737717
Nat Lubbock <natlubbock@Nats-MacBook-Air.local>
2020-09-18 12:52:14 +0100

    added AdminPickListAdapter.java and TeamPlayer.java

---
+++ app/src/main/java/com/example/ocrugbyapp/teams/AdminPickListAdapter.java
@@ -0,0 +1,24 @@
+package com.example.ocrugbyapp.teams;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdminPickListAdapter extends RecyclerView.Adapter<AdminPickListAdapter.PlayersVH> {
+    @NonNull
+    @Override
+    public AdminPickListAdapter.PlayersVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdminPickListAdapter.PlayersVH holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/ocrugbyapp/teams/TeamPlayer.java
@@ -0,0 +1,4 @@
+package com.example.ocrugbyapp.teams;
+
+public class TeamPlayer {
+}
