503edec1ed9dcdaaaa62b997292793a474c61253
Nat Lubbock <natlubbock@Nats-MacBook-Air.local>
2020-09-18 12:57:08 +0100

    set up TeamPlayer.java by adding constructor and getters and setters

--- app/src/main/java/com/example/ocrugbyapp/teams/TeamPlayer.java
+++ app/src/main/java/com/example/ocrugbyapp/teams/TeamPlayer.java
@@ -1,4 +1,18 @@
 package com.example.ocrugbyapp.teams;
 
 public class TeamPlayer {
+
+    private String userID;
+
+    public TeamPlayer(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
 }
