47e714d6f2f1f7f18b4c46fc83d84dc6f3c8e101
Nat Lubbock <nat.lubbock@gmail.com>
2020-10-22 11:47:34 +0300

    added resultsCard.java to store results information

--- app/src/main/java/com/example/ocrugbyapp/results/FirstsResults.java
+++ app/src/main/java/com/example/ocrugbyapp/results/FirstsResults.java
@@ -11,11 +11,15 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ocrugbyapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class FirstsResults extends Fragment {
 
     RecyclerView recyclerView;
+    FirebaseAuth mAuth;
+    FirebaseFirestore mStore;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,6 +28,9 @@ public class FirstsResults extends Fragment {
         View view = inflater.inflate(R.layout.fragment_firsts_results, container, false);
 
         recyclerView = view.findViewById(R.id.resultsList);
+
+        mAuth = FirebaseAuth.getInstance();
+        mStore = FirebaseFirestore.getInstance();
 
         return view;
     }
---
+++ app/src/main/java/com/example/ocrugbyapp/results/resultsCard.java
@@ -0,0 +1,64 @@
+package com.example.ocrugbyapp.results;
+
+public class resultsCard {
+
+    private String date, homeTeam, awayTeam, homeScore, awayScore;
+    private int fixtureNum;
+
+    public resultsCard(int fixtureNum, String date, String homeTeam, String awayTeam, String homeScore, String awayScore) {
+        this.fixtureNum = fixtureNum;
+        this.date = date;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.homeScore = homeScore;
+        this.awayScore = awayScore;
+    }
+
+    public int getFixtureNum() {
+        return fixtureNum;
+    }
+
+    public void setFixtureNum(int fixtureNum) {
+        this.fixtureNum = fixtureNum;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public String getHomeScore() {
+        return homeScore;
+    }
+
+    public void setHomeScore(String homeScore) {
+        this.homeScore = homeScore;
+    }
+
+    public String getAwayScore() {
+        return awayScore;
+    }
+
+    public void setAwayScore(String awayScore) {
+        this.awayScore = awayScore;
+    }
+}
