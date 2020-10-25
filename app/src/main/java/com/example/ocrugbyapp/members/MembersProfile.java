6dd662f3f651843a6fcce64eabcadacb5ebe70dc
Nat Lubbock <nat.lubbock@gmail.com>
2020-10-25 21:00:09 +0000

    added a button in the members profile that allows them to be added as an admin

--- app/src/main/java/com/example/ocrugbyapp/members/MembersProfile.java
+++ app/src/main/java/com/example/ocrugbyapp/members/MembersProfile.java
@@ -5,11 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ocrugbyapp.R;
@@ -20,7 +23,11 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
@@ -36,6 +43,7 @@ public class MembersProfile extends AppCompatActivity {
     ImageView profilePic, backBtn;
     StorageReference mStorageRef;
     ProgressBar progressBar;
+    Button adminBtn;
 
     @Override
     protected void onStart() {
@@ -63,6 +71,7 @@ public class MembersProfile extends AppCompatActivity {
         profilePic = findViewById(R.id.profilePic);
         backBtn = findViewById(R.id.backBtn);
         progressBar = findViewById(R.id.progressBar);
+        adminBtn = findViewById(R.id.adminBtn);
 
 
         mAuth = FirebaseAuth.getInstance();
@@ -104,10 +113,43 @@ public class MembersProfile extends AppCompatActivity {
                                     preferred_position.setText(document.getString("PreferredPosition"));
                                     secondPosition.setText(document.getString("SecondPosition"));
                                     thirdPosition.setText(document.getString("ThirdPosition"));
-                                }
 
+                                    progressBar.setVisibility(View.GONE);
+
+                                    String currentUser = mAuth.getCurrentUser().getUid();
+
+                                    DocumentReference documentReference2 = mStore.collection("users").document(currentUser);
+                                    documentReference2.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                                        @Override
+                                        public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                                            if (value.get("Admin").equals(true)) {
+                                                DocumentReference documentReference3 = mStore.collection("users").document(doc_id);
+
+                                                documentReference3.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                                                    @Override
+                                                    public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                                                        if (value.get("Admin").equals(false)) {
+                                                            adminBtn.setVisibility(View.VISIBLE);
+                                                            adminBtn.setOnClickListener(new View.OnClickListener() {
+                                                                @Override
+                                                                public void onClick(View v) {
+                                                                    documentReference3.update("Admin", true).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                        @Override
+                                                                        public void onSuccess(Void aVoid) {
+                                                                            Toast.makeText(MembersProfile.this, "member has successfully been added as an admin", Toast.LENGTH_SHORT).show();
+                                                                            adminBtn.setVisibility(View.GONE);
+                                                                        }
+                                                                    });
+                                                                }
+                                                            });
+                                                        }
+                                                    }
+                                                });
+                                            }
+                                        }
+                                    });
+                                }
                             }
-                            progressBar.setVisibility(View.GONE);
                         }else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
